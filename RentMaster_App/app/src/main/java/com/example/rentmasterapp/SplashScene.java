2b5ce01ebca9fe6989049ab6b842e1b403f6b4ed
NoFearAndrew <21520712@gm.uit.edu.vn>
2023-10-04 15:09:50 +0700

    app
    first update

---
+++ RentMaster_App/app/src/main/java/com/example/rentmasterapp/SplashScene.java
@@ -0,0 +1,23 @@
+package com.example.rentmasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+public class SplashScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_scene);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScene.this,LoginNumberScene.class));
+                finish();
+            }
+        },3000);
+    }
+}
